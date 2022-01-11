d367dd37fc36393bee580967e2c7dcdd6c3aee86
bleezebub <rishabhjain9024@gmail.com>
2022-01-12 03:14:09 +0530

    file update

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,11 +1,21 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Application  {
+  public void greet(){
+    List<String> greetings = new ArrayList<>();
+    greetings.add("Hello");
+    for(String gr: greetings){
+      System.out.println("Greeting: "+gr);
+    }
+  }
     public Application() {
         System.out.println ("Inside Application");
     }
     public static void main (String[] args) {
     	 System.out.println ("Starting Application");
 	     Application app = new Application();
+       app.greet();
     }
 }
